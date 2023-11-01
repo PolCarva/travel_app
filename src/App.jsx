e824062a729dd044be212fdb171b7a74b6e83a64
PolCarva <pablocarvalhogimenez@gmail.com>
2023-11-01 17:19:51 -0300

    functioning map

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import LocationIcon from "./components/LocationIcon";
-
+import Map from "./components/Map";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1 className="bg-black hover:bg-gray-10 text-white">Hello World</h1>
-      <LocationIcon category={"hotel"}/>
+      <Header />
+      <Map />
     </>
   );
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import GoogleMapReact from 'google-map-react'
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 const Map = () => {
-  return (
-    <div>
-      <GoogleMapReact/>
-    </div>
-  )
-}
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+  });
 
-export default Map
+  const coordinates = { lat: -34.90309469570489, lng: -56.15626041660778 };
+
+  if (loadError) return <div>Error loading map</div>;
+  if (!isLoaded) return <div>Loading...</div>;
+  return (
+    <div className="w-full bg-red-50 h-full flex-1">
+      <GoogleMap
+        center={coordinates}
+        mapContainerClassName="w-100 h-[100svh]"
+        zoom={17}
+      >
+        <Marker position={coordinates} />
+      </GoogleMap>
+    </div>
+  );
+};
+
+export default Map;
