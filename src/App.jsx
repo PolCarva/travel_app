e8d1e0d4b056796380b07b8b4cdb469ff60004ca
PolCarva <pablocarvalhogimenez@gmail.com>
2023-11-01 21:02:56 -0300

    accepted changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import DetailPage from "./pages/DetailPage";
 import ErrorPage from "./pages/ErrorPage";
-=======
-import Map from "./components/Map";
-import Header from "./components/Header";
->>>>>>> main
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Router>
         <Routes>
           <Route path="/" element={<MainPage />} />
@@ -19,10 +13,6 @@ function App() {
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </Router>
-=======
-      <Header />
-      <Map />
->>>>>>> main
     </>
   );
 }
