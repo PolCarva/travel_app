8d770b9d527a5ca85eb27ca5fba1941754496607
PolCarva <pablocarvalhogimenez@gmail.com>
2023-11-11 21:18:02 -0300

    Update ErrorPage component with fixed height

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -4,7 +4,7 @@ import { ImCompass2 } from "react-icons/im";
 
 const ErrorPage = () => {
   return (
-    <div className="h-screen overflow-hidden relative">
+    <div className="h-[100svh] overflow-hidden relative">
       <div className="h-full flex flex-col justify-center items-center text-center relative z-10">
         <h1 className="font-black text-gray-100 text-9xl">404</h1>
         <p className="text-2xl font-bold tracking-tight text-black sm:text-4xl">Uh-oh!</p>
